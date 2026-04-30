70bd8238713093c6bd44aeaeabddae1081c3fc0e
Abdul Rehman Butt <arbutt4443@gmail.com>
2026-04-30 09:03:07 +0500

    implemented database.js to connect DB

---
+++ src/config/database.js
@@ -0,0 +1,25 @@
+const sql = require('mssql');
+require('dotenv').config();
+
+const config = {
+    server: process.env.DB_SERVER,
+    database: process.env.DB_NAME,
+    options: {
+        trustedConnection: true,
+        trustServerCertificate: true
+    }
+};
+
+const poolPromise = new sql.ConnectionPool(config)
+    .connect()
+    .then(pool => {
+        console.log('Connection Secured with SQL Server');
+        return pool;
+    })
+    .catch(err => {
+        console.log('Connection Failed: ', err);
+    });
+
+module.exports = {
+    sql, poolPromise
+};
