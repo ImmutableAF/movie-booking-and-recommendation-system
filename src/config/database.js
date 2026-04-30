feff974cfb8c417f52505133ca93ae76a5b51c45
Abdul Rehman Butt <arbutt4443@gmail.com>
2026-05-01 00:57:15 +0500

    implented server connection, application layer in between server and browser

--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const config = {
     server: process.env.DB_SERVER,
     database: process.env.DB_NAME,
+    driver: 'msnodesqlv8',
     options: {
         trustedConnection: true,
         trustServerCertificate: true
